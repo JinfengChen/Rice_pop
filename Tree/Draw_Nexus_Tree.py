d537c3a6fb1a68992450ba2795d838e5c256b7b4
JinfengChen <jinfeng7chen@gmail.com>
2015-04-07 14:21:30 -0700

    fix mPing value and layout of barplot

--- Tree/Draw_Nexus_Tree.py
+++ Tree/Draw_Nexus_Tree.py
@@ -11,8 +11,8 @@ import pylab
 def usage():
     test="name"
     message='''
-python Draw_Nexus_Tree.py --input figtree/test.phy.nexus.newick --anno figtree/test.phy.anno --col 7
-python Draw_Nexus_Tree.py --input test.phy.nexus.newick --anno test.phy.anno --col 4
+python Draw_Nexus_Tree.py --input figtree/test.phy.nexus.newick --anno figtree/test.phy.anno --col 7 --output test.phy
+python Draw_Nexus_Tree.py --input test.phy.nexus.newick --anno test.phy.anno --col 4 --output test.phy
 
 Plot newick phylogenetic tree with trait values in barplot using phytools in R.
 http://blog.phytools.org/2014/05/new-version-of-plottreewbars-that.html
@@ -28,7 +28,7 @@ Delta	UK	gray
 Epsilon	EPS	blue
 Alpha	US	red
 --col: columen to draw trait
-
+--output: output prefix of R and pdf
     '''
     print message
 
@@ -50,7 +50,7 @@ def readtable(infile):
                     data[unit[0]] = unit[1]
     return data
 
-def write_R(newick, anno, col, output):
+def write_R(newick, anno, col, prefix):
 
     R_cmd='''
 library("ape")
@@ -59,29 +59,31 @@ x = read.table(file="%s", sep='\\t', header=1)
 y = setNames(x[,%s], x[,1])
 y = y[match(tree$tip.label, names(y))]
 
-pdf("test.pdf")
+pdf("%s.pdf")
 layout(matrix(c(1,2),1,2),c(0.7,0.3))
 #par(mfrow=c(1,2))
 #par(mar=c(4.1,0,1.1,1.1))
+par(mar=c(4,1,2,2))
 #plotTree(tree, mar=c(5.1,2,1.1,1.1))
 #plot(tree,edge.color=c('red','green','red','gray','orange'))
 plot(tree, show.tip.label = FALSE)
 #plot(tree)
 #par(mar=c(5.1,2,1.1,1.1))
 barplot(y,horiz=TRUE,width=1,space=0, xlim=c(-10, 200),  ylim=c(0.5,length(tree$tip.label)),names="", axes=FALSE)
-axis(1, at= c(0, 100, 200), line=3)
+axis(1, at= c(0, 100, 200), line=1)
 dev.off()
-''' %(newick, anno, col)
-    ofile = open (output, 'w')
+''' %(newick, anno, col, prefix)
+    ofile = open ('%s.R' %(prefix), 'w')
     print >> ofile, R_cmd
     ofile.close()
-    os.system('cat %s | R --slave' %(output))
+    os.system('cat %s.R | R --slave' %(prefix))
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input')
     parser.add_argument('-a', '--anno')
     parser.add_argument('-c', '--col')
+    parser.add_argument('-o', '--output')
     parser.add_argument('-v', dest='verbose', action='store_true')
     args = parser.parse_args()
     try:
@@ -94,7 +96,7 @@ def main():
     tree_file = args.input
     anno_file = args.anno
     colume    = args.col
-    write_R(tree_file, anno_file, colume, 'tree_trait.R')    
+    write_R(tree_file, anno_file, colume, args.output)    
 
 if __name__ == '__main__':
     main()
--- Tree/work.sh
+++ Tree/work.sh
@@ -7,9 +7,9 @@ perl /rhome/cjinfeng/software/bin/fastaDeal.pl --attr id:len 3K_coreSNP-v2.1.pru
 
 
 echo "annotaion tree, ALL_3000"
-python Anno_Nexus_tree.py --input 3K_coreSNP-v2.1.pruneddata.tab.fasttree.tree --format newick --anno ../GigaScience/rice_line_ALL_3000.anno.list --color bl
-python Anno_Nexus_tree.py --input 3K_coreSNP-v2.1.pruneddata.tab.fasttree.nj.tree --format newick --anno ../GigaScience/rice_line_ALL_3000.anno.list --color bl
+python Anno_Nexus_tree.py --input 3K_coreSNP-v2.1.pruneddata.tab.fasttree.tree --format newick --anno ../GigaScience/rice_line_ALL_3000.anno.figtree.list --color bl
+python Anno_Nexus_tree.py --input 3K_coreSNP-v2.1.pruneddata.tab.fasttree.nj.tree --format newick --anno ../GigaScience/rice_line_ALL_3000.anno.figtree.list --color bl
 
 echo "Draw tree with trait"
-python Draw_Nexus_Tree.py --input 3K_coreSNP-v2.1.pruneddata.tab.fasttree.nj.tree --anno rice_line_ALL_3000.anno.list --col 7
+python Draw_Nexus_Tree.py --input 3K_coreSNP-v2.1.pruneddata.tab.fasttree.nj.tree --anno rice_line_ALL_3000.anno.R.list --col 7 --output 3K_coreSNP-v2.1.pruneddata.tab.fasttree.nj.tree
 
