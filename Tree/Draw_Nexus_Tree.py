957d24d0ebf7bbdd3c441acf546c111b6a6cffda
JinfengChen <jinfeng7chen@gmail.com>
2015-04-09 21:47:14 -0700

    add legend

--- Tree/Draw_Nexus_Tree.py
+++ Tree/Draw_Nexus_Tree.py
@@ -176,7 +176,7 @@ sample_colors = as.vector(sample_colors)
 
 pdf("%s.pdf")
 layout(matrix(c(1,2),1,2),c(0.7,0.3))
-par(mar=c(4,1,2,2))
+par(mar=c(4,1,2,4))
 edge_colors=rep("black", length(tree$edge[,2]))
 
 #https://ecomorph.wordpress.com/2014/10/09/phylogenetic-trees-in-r-4/
@@ -192,7 +192,16 @@ for (i in 1:length(edge_num)){
 }
 
 plot(tree, edge.color=edge_colors, show.tip.label = %s)
+leg_inf = cbind(as.vector(x[,2]), as.vector(x[,6]))
+leg_inf = unique(leg_inf)
+leg_inf = leg_inf[order(leg_inf[,2]),]
+xrange  = par("xaxp")
+yrange  = par("yaxp")
+par(xpd=TRUE) #set this legend can be plot into margin area
+#legend(x=xrange[2]*0.8, y=yrange[2]*0.99, substr(leg_inf[,2], 1, 4), fill=leg_inf[,1], border=FALSE, bty='n')
+legend(x=xrange[2]*0.8, y=yrange[2]*0.99, leg_inf[,2], fill=leg_inf[,1], border=FALSE, bty='n')
 
+par(mar=c(4,0.5, 2, 1)) #set left and right to be tight with other plot
 barplot(y,horiz=TRUE,width=1,space=0, xlim=c(-10, 200),  ylim=c(0.5,length(tree$tip.label)),names="", axes=FALSE)
 axis(1, at= c(0, 100, 200), line=1)
 dev.off()
